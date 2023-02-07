c2f75d0351fd236049fd0b9b3a2a55f97075d613
RicardoAndresGutierrez <rianguma@gmail.com>
2023-02-06 22:12:01 -0300

    modify3

--- src/pages/NosotrosPage.js
+++ src/pages/NosotrosPage.js
@@ -17,50 +17,55 @@ const NosotrosPage = (props) => {
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, illum quod quos eaque doloremque
                 ut, commodi ab consequuntur mollitia dolore vero aperiam sit impedit incidunt culpa expedita. Labore,
                 deserunt porro?</p>
-                <hr/>
+            <hr />
         </div>
-        <Card className="cardstaff" border="warning">
-            <Card.Img variant="top" src="imagenes/staff/nacho.jpg" />
-            <Card.Body>
-                <Card.Title className="title">Ignacio</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in
-                    to additional content. This content is a little bit longer.
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">Paracaidista</small>
-            </Card.Footer>
-        </Card>
-        <Card className="cardstaff" border="warning">
-            <Card.Img variant="top" src="imagenes/staff/robert.jpg" />
-            <Card.Body>
-                <Card.Title className="title">Roberto</Card.Title>
-                <Card.Text>
-                    This card has supporting text below as a natural lead-in to
-                    additional content.{' '}
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">Paracaidista</small>
-            </Card.Footer>
-        </Card>
+        <div className="cards">
+            <Card className="cardstaff" border="warning" width="10px" >
+                <Card.Img variant="top" src="imagenes/staff/nacho.jpg" />
+                <Card.Body>
+                    <Card.Title className="title">Ignacio</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Paracaidista</small>
+                </Card.Footer>
+            </Card>
 
-        <Card className="cardstaff" border="warning">
-            <Card.Img variant="top" src="imagenes/staff/andres.jpg" />
-            <Card.Body>
-                <Card.Title className="title">Andrés</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in
-                    to additional content. This card has even longer content than the
-                    first to show that equal height action.
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">Paracaidista</small>
-            </Card.Footer>
-        </Card>
+            <Card className="cardstaff" border="warning">
+                <Card.Img variant="top" src="imagenes/staff/robert.jpg" />
+                <Card.Body>
+                    <Card.Title className="title">Roberto</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to
+                        additional content.{' '}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Paracaidista</small>
+                </Card.Footer>
+            </Card>
+
+            <Card className="cardstaff" border="warning">
+                <Card.Img variant="top" src="imagenes/staff/andres.jpg" />
+                <Card.Body>
+                    <Card.Title className="title">Andrés</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This card has even longer content than the
+                        first to show that equal height action.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Paracaidista</small>
+                </Card.Footer>
+            </Card>
+
+        </div>
     </CardGroup>
+
 
     )
 }
